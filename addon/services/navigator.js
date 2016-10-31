28b13eb458d8697d3f287125707d72cb9d47ea67
Ken Snyder <ken@ken.net>
2016-10-31 21:11:29 +0000

    added some defensive coding

--- addon/services/navigator.js
+++ addon/services/navigator.js
@@ -15,19 +15,20 @@ const navigatorService = Ember.Service.extend({
     return leaf === 'index' ? chain.pop() : leaf;
   }),
   isIndexRoute: computed('currentPath', function() {
-    return this.get('currentPath').split('.').pop() === 'index';
+    const currentPath = this.get('currentPath') || [];
+    return currentPath.split('.').pop() === 'index';
   }),
   primaryRoute: computed('currentPath', function() {
-    const currentPath = this.get('currentPath').split('.');
+    const currentPath = (this.get('currentPath') || []).split('.');
     return currentPath[0] !== 'index' ? currentPath[0] : null;
   }),
   secondaryRoute: computed('currentPath', function() {
-    const currentPath = this.get('currentPath').split('.');
+    const currentPath = (this.get('currentPath') || []).split('.');
     const length = currentPath.length;
     return currentPath[1] !== 'index' && length > 1 ? currentPath[1] : null;
   }),
   routeParts: computed('currentPath', function() {
-    return this.get('currentPath').split('.').filter(p => p !== 'index');
+    return (this.get('currentPath') || []).split('.').filter(p => p !== 'index');
   }),
   routeContexts: computed('currentPath', '_contextMutex', function() {
     const parts = this.get('routeParts');
